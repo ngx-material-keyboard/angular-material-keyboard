326fc1736a3fb2f9cc03673cd186cc12758da81f
David Enke <post@davidenke.de>
2016-02-14 17:02:42 +0100

    simple keys work now

--- dist/mdKeyboard.js
+++ dist/mdKeyboard.js
@@ -62,54 +62,54 @@ angular
     .config(function ($mdThemingProvider) {
     });
 
-/* See http://www.greywyvern.com/code/javascript/keyboard for examples
- * and usage instructions.
- *
- * Version 1.49 - November 8, 2011
- *   - Don't display language drop-down if only one keyboard available
- *
- *   See full changelog at:
- *     http://www.greywyvern.com/code/javascript/keyboard.changelog.txt
- *
- * Keyboard Credits
- *   - Yiddish (Yidish Lebt) keyboard layout by Simche Taub (jidysz.net)
- *   - Urdu Phonetic keyboard layout by Khalid Malik
- *   - Yiddish keyboard layout by Helmut Wollmersdorfer
- *   - Khmer keyboard layout by Sovann Heng (km-kh.com)
- *   - Dari keyboard layout by Saif Fazel
- *   - Kurdish keyboard layout by Ara Qadir
- *   - Assamese keyboard layout by Kanchan Gogoi
- *   - Bulgarian BDS keyboard layout by Milen Georgiev
- *   - Basic Japanese Hiragana/Katakana keyboard layout by Damjan
- *   - Ukrainian keyboard layout by Dmitry Nikitin
- *   - Macedonian keyboard layout by Damjan Dimitrioski
- *   - Pashto keyboard layout by Ahmad Wali Achakzai (qamosona.com)
- *   - Armenian Eastern and Western keyboard layouts by Hayastan Project (www.hayastan.co.uk)
- *   - Pinyin keyboard layout from a collaboration with Lou Winklemann
- *   - Kazakh keyboard layout by Alex Madyankin
- *   - Danish keyboard layout by Verner KjÃ¦rsgaard
- *   - Slovak keyboard layout by Daniel Lara (www.learningslovak.com)
- *   - Belarusian and Serbian Cyrillic keyboard layouts by Evgeniy Titov
- *   - Bulgarian Phonetic keyboard layout by Samuil Gospodinov
- *   - Swedish keyboard layout by HÃ¥kan Sandberg
- *   - Romanian keyboard layout by Aurel
- *   - Farsi (Persian) keyboard layout by Kaveh Bakhtiyari (www.bakhtiyari.com)
- *   - Burmese keyboard layout by Cetanapa
- *   - Bosnian/Croatian/Serbian Latin/Slovenian keyboard layout by Miran Zeljko
- *   - Hungarian keyboard layout by Antal Sall 'Hiromacu'
- *   - Arabic keyboard layout by Srinivas Reddy
- *   - Italian and Spanish (Spain) keyboard layouts by dictionarist.com
- *   - Lithuanian and Russian keyboard layouts by Ramunas
- *   - German keyboard layout by QuHno
- *   - French keyboard layout by Hidden Evil
- *   - Polish Programmers layout by moose
- *   - Turkish keyboard layouts by offcu
- *   - Dutch and US Int'l keyboard layouts by jerone
- *
- */
+    /* See http://www.greywyvern.com/code/javascript/keyboard for examples
+     * and usage instructions.
+     *
+     * Version 1.49 - November 8, 2011
+     *   - Don't display language drop-down if only one keyboard available
+     *
+     *   See full changelog at:
+     *     http://www.greywyvern.com/code/javascript/keyboard.changelog.txt
+     *
+     * Keyboard Credits
+     *   - Yiddish (Yidish Lebt) keyboard layout by Simche Taub (jidysz.net)
+     *   - Urdu Phonetic keyboard layout by Khalid Malik
+     *   - Yiddish keyboard layout by Helmut Wollmersdorfer
+     *   - Khmer keyboard layout by Sovann Heng (km-kh.com)
+     *   - Dari keyboard layout by Saif Fazel
+     *   - Kurdish keyboard layout by Ara Qadir
+     *   - Assamese keyboard layout by Kanchan Gogoi
+     *   - Bulgarian BDS keyboard layout by Milen Georgiev
+     *   - Basic Japanese Hiragana/Katakana keyboard layout by Damjan
+     *   - Ukrainian keyboard layout by Dmitry Nikitin
+     *   - Macedonian keyboard layout by Damjan Dimitrioski
+     *   - Pashto keyboard layout by Ahmad Wali Achakzai (qamosona.com)
+     *   - Armenian Eastern and Western keyboard layouts by Hayastan Project (www.hayastan.co.uk)
+     *   - Pinyin keyboard layout from a collaboration with Lou Winklemann
+     *   - Kazakh keyboard layout by Alex Madyankin
+     *   - Danish keyboard layout by Verner KjÃ¦rsgaard
+     *   - Slovak keyboard layout by Daniel Lara (www.learningslovak.com)
+     *   - Belarusian and Serbian Cyrillic keyboard layouts by Evgeniy Titov
+     *   - Bulgarian Phonetic keyboard layout by Samuil Gospodinov
+     *   - Swedish keyboard layout by HÃ¥kan Sandberg
+     *   - Romanian keyboard layout by Aurel
+     *   - Farsi (Persian) keyboard layout by Kaveh Bakhtiyari (www.bakhtiyari.com)
+     *   - Burmese keyboard layout by Cetanapa
+     *   - Bosnian/Croatian/Serbian Latin/Slovenian keyboard layout by Miran Zeljko
+     *   - Hungarian keyboard layout by Antal Sall 'Hiromacu'
+     *   - Arabic keyboard layout by Srinivas Reddy
+     *   - Italian and Spanish (Spain) keyboard layouts by dictionarist.com
+     *   - Lithuanian and Russian keyboard layouts by Ramunas
+     *   - German keyboard layout by QuHno
+     *   - French keyboard layout by Hidden Evil
+     *   - Polish Programmers layout by moose
+     *   - Turkish keyboard layouts by offcu
+     *   - Dutch and US Int'l keyboard layouts by jerone
+     *
+     */
 
-angular
-    .module('material.components.keyboard')
+    angular
+        .module('material.components.keyboard')
     .constant('keyboardLayouts', (function () {
         var layouts = {
             '\u0627\u0644\u0639\u0631\u0628\u064a\u0629': {
@@ -1327,7 +1327,7 @@ function MdKeyboardDirective($mdKeyboard, $mdTheming) {
     };
 }
 
-function useKeyboardDirective($mdKeyboard, $injector, $rootScope) {
+    function useKeyboardDirective($mdKeyboard, $injector, $rootScope) {
     return {
         restrict: 'A',
         require: '?ngModel',
@@ -1360,7 +1360,7 @@ function useKeyboardDirective($mdKeyboard, $injector, $rootScope) {
             function showKeyboard() {
                 if (!keyboard) {
                     keyboard = $mdKeyboard.show({
-                        template:'<md-keyboard class=md-grid layout=column ng-cloak><div ng-repeat="row in keyboard.keys" layout=row><div flex ng-repeat="key in row"><md-button ng-switch=key[0] class=md-raised ng-mousedown="pressed($event, key)" aria-label={{key[0]}}><md-icon ng-switch-when=Bksp md-svg-icon=hardware:keyboard_backspace></md-icon><md-icon ng-switch-when=Tab md-svg-icon=hardware:keyboard_tab></md-icon><md-icon ng-switch-when=Caps md-svg-icon=hardware:keyboard_capslock></md-icon><md-icon ng-switch-when=Enter md-svg-icon=hardware:keyboard_return></md-icon><span ng-switch-default>{{key[0]}}</span></md-button></div></div></md-keyboard>',
+                        template: '<md-keyboard class=md-grid layout=column ng-cloak><div ng-repeat="row in keyboard.keys" layout=row><div flex ng-repeat="key in row"><md-button ng-switch=key[0] class=md-raised ng-mousedown="pressed($event, key)" aria-label={{key[0]}}><md-icon ng-switch-when=Bksp md-svg-icon=hardware:keyboard_backspace></md-icon><md-icon ng-switch-when=Tab md-svg-icon=hardware:keyboard_tab></md-icon><md-icon ng-switch-when=Caps md-svg-icon=hardware:keyboard_capslock></md-icon><md-icon ng-switch-when=Enter md-svg-icon=hardware:keyboard_return></md-icon><span ng-switch-default>{{key[0]}}</span></md-button></div></div></md-keyboard>',
                         controller: function mdKeyboardCtrl($scope) {
                             this.resolve = function () {
                                 $mdKeyboard.hide('ok');
