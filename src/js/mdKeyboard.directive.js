86bf6388b2e23f30e0e51f2b44abd18cd87ad6d3
David Enke <david.enke@zalari.de>
2016-05-24 13:01:06 +0200

    use 'triggerKeys' for all keys

--- src/js/mdKeyboard.directive.js
+++ src/js/mdKeyboard.directive.js
@@ -72,20 +72,10 @@ function useKeyboardDirective($mdKeyboard, $timeout, $animate, $rootScope) {
             }
 
             function mdKeyboardController($scope) {
-                if (attrs.useKeyboard) {
-                    $mdKeyboard.useLayout(attrs.useKeyboard);
-                }
-
-                var toggleCaps = function () {
-                    $scope.caps = !$scope.caps;
-                };
-
-                var toggleCapsLock = function () {
-                    $scope.capsLocked = !$scope.capsLocked;
-                };
+                $mdKeyboard.useLayout(attrs.useKeyboard);
 
                 var getKeyClass = function (key) {
-                    var k = key[0].toLowerCase();
+                    var k = (key[0] || ' ').toLowerCase();
                     var keys = ['bksp', 'tab', 'caps', 'enter', 'shift', 'alt', 'altgr', 'altlk'];
 
                     // space bar
@@ -104,14 +94,114 @@ function useKeyboardDirective($mdKeyboard, $timeout, $animate, $rootScope) {
                     return 'key-' + k;
                 };
 
+                var triggerKey = function($event, key) {
+                    $event.preventDefault();
+
+                    switch (key) {
+                        case "Caps":
+                            $scope.capsLocked = !$scope.capsLocked;
+                            $scope.caps = false;
+                            break;
+                        
+                        case "Shift":
+                            $scope.caps = !$scope.caps;
+                            break;
+                        
+                        case "Alt":
+                        case "AltGr":
+                        case "AltLk":
+                            // modify input, visualize
+                            //self.VKI_modify(type);
+                            break;
+
+                        case "Tab":
+                            // TODO: handle text selection
+
+                            // cycle through elements
+                            // or insert \t tab
+                            //if (self.VKI_activeTab) {
+                            //    if (self.VKI_target.form) {
+                            //        var target = self.VKI_target, elems = target.form.elements;
+                            //        self.VKI_close(false);
+                            //        for (var z = 0, me = false, j = -1; z < elems.length; z++) {
+                            //            if (j == -1 && elems[z].getAttribute("VKI_attached")) j = z;
+                            //            if (me) {
+                            //                if (self.VKI_activeTab == 1 && elems[z]) break;
+                            //                if (elems[z].getAttribute("VKI_attached")) break;
+                            //            } else if (elems[z] == target) me = true;
+                            //        }
+                            //        if (z == elems.length) z = Math.max(j, 0);
+                            //        if (elems[z].getAttribute("VKI_attached")) {
+                            //            self.VKI_show(elems[z]);
+                            //        } else elems[z].focus();
+                            //    } else self.VKI_target.focus();
+                            //} else self.VKI_insert("\t");
+                            //return false;
+
+                            $mdKeyboard.currentModel.$setViewValue(($mdKeyboard.currentModel.$viewValue || '') + "\t");
+                            $mdKeyboard.currentModel.$validate();
+                            $mdKeyboard.currentModel.$render();
+
+                            break;
+
+                        case "Bksp":
+                            // TODO: handle text selection
+
+                            // backspace
+                            //self.VKI_target.focus();
+                            //if (self.VKI_target.setSelectionRange && hasSelectionStartEnd(self.VKI_target) && !self.VKI_target.readOnly) {
+                            //    var rng = [self.VKI_target.selectionStart, self.VKI_target.selectionEnd];
+                            //    if (rng[0] < rng[1]) rng[0]++;
+                            //    self.VKI_target.value = self.VKI_target.value.substr(0, rng[0] - 1) + self.VKI_target.value.substr(rng[1]);
+                            //    self.VKI_target.setSelectionRange(rng[0] - 1, rng[0] - 1);
+                            //} else if (self.VKI_target.createTextRange && !self.VKI_target.readOnly) {
+                            //    try {
+                            //        self.VKI_target.range.select();
+                            //    } catch (e) {
+                            //        self.VKI_target.range = document.selection.createRange();
+                            //    }
+                            //    if (!self.VKI_target.range.text.length) self.VKI_target.range.moveStart('character', -1);
+                            //    self.VKI_target.range.text = "";
+                            //} else self.VKI_target.value = self.VKI_target.value.substr(0, self.VKI_target.value.length - 1);
+                            //if (self.VKI_shift) self.VKI_modify("Shift");
+                            //if (self.VKI_altgr) self.VKI_modify("AltGr");
+                            //self.VKI_target.focus();
+                            //self.keyInputCallback();
+                            //return true;
+
+                            $mdKeyboard.currentModel.$setViewValue(($mdKeyboard.currentModel.$viewValue || '').slice(0, -1));
+                            $mdKeyboard.currentModel.$validate();
+                            $mdKeyboard.currentModel.$render();
+
+                            break;
+
+                        case "Enter":
+                            if (element[0].nodeName.toUpperCase() != 'TEXTAREA') {
+                                $timeout(function () {
+                                    angular.element(element[0].form).triggerHandler('submit');
+                                });
+                            } else {
+                                $mdKeyboard.currentModel.$setViewValue(($mdKeyboard.currentModel.$viewValue || '') + "\n");
+                                $mdKeyboard.currentModel.$validate();
+                                $mdKeyboard.currentModel.$render();
+                            }
+
+                            break;
+
+                        default:
+                            $mdKeyboard.currentModel.$setViewValue(($mdKeyboard.currentModel.$viewValue || '') + key[0]);
+                            $mdKeyboard.currentModel.$validate();
+                            $mdKeyboard.currentModel.$render();
+
+                            $scope.caps = false;
+                    }
+                };
+
                 var _init = function () {
                     $scope.resolve = function () {
                         $mdKeyboard.hide('ok');
                     };
                     $scope.getKeyClass = getKeyClass;
-                    $scope.keyboard = $mdKeyboard.getLayout();
-                    $scope.toggleCaps = toggleCaps;
-                    $scope.toggleCapsLock = toggleCapsLock;
                     $scope.keyboard = $mdKeyboard.getCurrentLayout();
                     $scope.pressed = triggerKey;
 
@@ -133,115 +223,6 @@ function useKeyboardDirective($mdKeyboard, $timeout, $animate, $rootScope) {
                     var selLen = document.selection.createRange().text.length;
                     sel.moveStart('character', -element.value.length);
                     return sel.text.length - selLen;
-                }
-            };
-
-            function triggerKey($event, key) {
-                $event.preventDefault();
-                $log.info('key pressed: %s (%s)', key, key.charCodeAt(0));
-
-                switch (key) {
-                    case "Caps":
-                    case "Shift":
-                    case "Alt":
-                    case "AltGr":
-                    case "AltLk":
-                        // modify input, visualize
-                        //self.VKI_modify(type);
-                        break;
-
-                    case "Tab":
-                        // TODO: handle text selection
-
-                        // cycle through elements
-                        // or insert \t tab
-                        //if (self.VKI_activeTab) {
-                        //    if (self.VKI_target.form) {
-                        //        var target = self.VKI_target, elems = target.form.elements;
-                        //        self.VKI_close(false);
-                        //        for (var z = 0, me = false, j = -1; z < elems.length; z++) {
-                        //            if (j == -1 && elems[z].getAttribute("VKI_attached")) j = z;
-                        //            if (me) {
-                        //                if (self.VKI_activeTab == 1 && elems[z]) break;
-                        //                if (elems[z].getAttribute("VKI_attached")) break;
-                        //            } else if (elems[z] == target) me = true;
-                        //        }
-                        //        if (z == elems.length) z = Math.max(j, 0);
-                        //        if (elems[z].getAttribute("VKI_attached")) {
-                        //            self.VKI_show(elems[z]);
-                        //        } else elems[z].focus();
-                        //    } else self.VKI_target.focus();
-                        //} else self.VKI_insert("\t");
-                        //return false;
-
-                        $mdKeyboard.currentModel.$setViewValue(($mdKeyboard.currentModel.$viewValue || '') + "\t");
-                        $mdKeyboard.currentModel.$validate();
-                        $mdKeyboard.currentModel.$render();
-
-                        break;
-
-                    case "Bksp":
-                        // TODO: handle text selection
-
-                        // backspace
-                        //self.VKI_target.focus();
-                        //if (self.VKI_target.setSelectionRange && hasSelectionStartEnd(self.VKI_target) && !self.VKI_target.readOnly) {
-                        //    var rng = [self.VKI_target.selectionStart, self.VKI_target.selectionEnd];
-                        //    if (rng[0] < rng[1]) rng[0]++;
-                        //    self.VKI_target.value = self.VKI_target.value.substr(0, rng[0] - 1) + self.VKI_target.value.substr(rng[1]);
-                        //    self.VKI_target.setSelectionRange(rng[0] - 1, rng[0] - 1);
-                        //} else if (self.VKI_target.createTextRange && !self.VKI_target.readOnly) {
-                        //    try {
-                        //        self.VKI_target.range.select();
-                        //    } catch (e) {
-                        //        self.VKI_target.range = document.selection.createRange();
-                        //    }
-                        //    if (!self.VKI_target.range.text.length) self.VKI_target.range.moveStart('character', -1);
-                        //    self.VKI_target.range.text = "";
-                        //} else self.VKI_target.value = self.VKI_target.value.substr(0, self.VKI_target.value.length - 1);
-                        //if (self.VKI_shift) self.VKI_modify("Shift");
-                        //if (self.VKI_altgr) self.VKI_modify("AltGr");
-                        //self.VKI_target.focus();
-                        //self.keyInputCallback();
-                        //return true;
-
-                        $mdKeyboard.currentModel.$setViewValue(($mdKeyboard.currentModel.$viewValue || '').slice(0, -1));
-                        $mdKeyboard.currentModel.$validate();
-                        $mdKeyboard.currentModel.$render();
-
-                        break;
-
-                    case "Enter":
-                        if (element[0].nodeName.toUpperCase() != 'TEXTAREA') {
-                            $timeout(function () {
-                                angular.element(element[0].form).triggerHandler('submit');
-                            });
-                        } else {
-                            $mdKeyboard.currentModel.$setViewValue(($mdKeyboard.currentModel.$viewValue || '') + "\n");
-                            $mdKeyboard.currentModel.$validate();
-                            $mdKeyboard.currentModel.$render();
-                        }
-
-                        break;
-
-                    default:
-                        // TODO: handle text selection
-
-                        //$timeout(function () {
-                        //var event = new window.KeyboardEvent('keypress', {
-                        //    bubbles: true,
-                        //    cancelable: true,
-                        //    shiftKey: true,
-                        //    keyCode: key.charCodeAt(0)
-                        //});
-                        // element[0].dispatchEvent(event);
-                        //});
-
-                        $mdKeyboard.currentModel.$setViewValue(($mdKeyboard.currentModel.$viewValue || '') + key[0]);
-                        $mdKeyboard.currentModel.$validate();
-                        $mdKeyboard.currentModel.$render();
-
-                        scope.caps = false;
                 }
             }
 
